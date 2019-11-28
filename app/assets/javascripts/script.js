40b845cc4ab444b70bebd783f31c6b7536bad41d
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-28 22:04:53 +0900

    Create script.js

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,23 @@
+$(function() {
+  //クリックしたときのファンクションをまとめて指定
+  $('.tab li').click(function(){
+    //.index()を使いクリックされたタブが何番目かを調べ、
+    //indexという変数に代入します。
+    var index = $('.tab li').index(this);
+    //コンテンツを一度消して、すべて非表示にし、
+    $('.content li').removeClass('hide');
+    $('.content li').css('display','none');
+    //クリックされたタブと同じ順番のコンテンツを表示します。
+    $('.content li').eq(index).css('display','block');
+    //一度タブについているクラスselectを消し、
+    $('.tab li').removeClass('select');
+    //クリックされたタブのみにクラスselectをつけます。
+    $(this).addClass('select')
+  });
+});
+
+$(function(){
+  $('.btn-default').click(function(){
+    $('.under_box').removeClass('create_baby');
+  });
+})
