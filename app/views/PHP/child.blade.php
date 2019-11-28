08b05bb24e349bbe711002478ed5bea6c3a8ace0
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-28 21:50:49 +0900

    Create child.blade.php

---
+++ app/views/PHP/child.blade.php
@@ -0,0 +1,14 @@
+
+@extends('baby.index')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @@parent
+
+    <p>ここはメインのサイドバーに追加される</p>
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツ</p>
+@endsection
