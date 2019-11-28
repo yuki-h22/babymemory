56a72b708d300042aa76bfb0de83f935e7110ec5
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-29 00:05:32 +0900

    Update 20191107085221_create_groups.rb

--- db/migrate/20191107085221_create_groups.rb
+++ db/migrate/20191107085221_create_groups.rb
@@ -2,6 +2,8 @@ class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.string :name, null: false
+      t.string :birthday, null: false
+      t.string :image
       t.timestamps
     end
   end
