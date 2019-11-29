fec60a5f50b1210745e1c740984e610fc07f102b
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-29 19:42:41 +0900

    Update 20191107085221_create_groups.rb

--- db/migrate/20191107085221_create_groups.rb
+++ db/migrate/20191107085221_create_groups.rb
@@ -4,6 +4,50 @@ class CreateGroups < ActiveRecord::Migration[5.0]
       t.string :name, null: false
       t.string :birthday, null: false
       t.string :image
+      t.string :mhug
+      t.string :phug
+      t.string :fphoto
+      t.string :imhome
+      t.string :finger
+      t.string :bathing
+      t.string :diapers
+      t.string :hund
+      t.string :walking
+      t.string :bathingwith1
+      t.string :sleepingface1
+      t.string :discovery
+      t.string :cream
+      t.string :saycheese1
+      t.string :neck
+      t.string :bathingwith2
+      t.string :get
+      t.string :tooth
+      t.string :roll
+      t.string :babyfood
+      t.string :brush
+      t.string :sleepingface2
+      t.string :saycheese2
+      t.string :high
+      t.string :mam
+      t.string :high2
+      t.string :looking
+      t.string :sit
+      t.string :stand
+      t.string :high3
+      t.string :straw
+      t.string :sleepingface3
+      t.string :saycheese3
+      t.string :imitate
+      t.string :go
+      t.string :hair
+      t.string :katikati
+      t.string :follow
+      t.string :pause
+      t.string :walk
+      t.string :yam
+      t.string :sleepingface4
+      t.string :saycheese4
+      t.string :imitate2
       t.timestamps
     end
   end
