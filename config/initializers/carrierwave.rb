23f1c4d34dbb9348258deecf7e055812a06659d6
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-12-01 16:15:07 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    provider: 'AWS',
+    provider: ' HEROKU',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
